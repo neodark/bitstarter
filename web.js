600ebed16161d9d516c407269b1b389bb8b9853a
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-07-10 10:26:10 +0200

    [project] Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fileToRead = "index.html";
+  var input = fs.readFileSync(fileToRead);
+  //response.send('Hello World 2!');
+  response.send(input);
 });
 
 var port = process.env.PORT || 5000;
